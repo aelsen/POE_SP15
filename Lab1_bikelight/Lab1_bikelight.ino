e8107a646df5396bc4ddda2fd174110d03479113
Antonia Elsen <antoniaelsen@gmail.com>
2015-01-26 15:03:40 -0500

    add bounce and flash functions, fix attachinterrupt

--- Lab1_bikelight/Lab1_bikelight.ino
+++ Lab1_bikelight/Lab1_bikelight.ino
@@ -1,38 +1,79 @@
 
 int button = 2;
+volatile int switcher = 0;
 boolean flash;
+int flashtime = 50;
+int bouncetime = 100;
 
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
-  pinMode(l1, OUTPUT);
+  pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(button, INPUT);
+  attachInterrupt(0, switcherf, RISING);
+}
+
+void switcherf() {
+  switcher++;
+  switcher = switcher % 4;
+  delay(500);
+  Serial.println("case: ");
+  Serial.println(switcher);
 }
 
 void loop() {
-  
+switch (switcher){
+  case 0:
+    allOn();
+    break;
+  case 1:
+    allOff();
+    break;
+  case 2:
+    delay(flashtime);
+    flash = !flash;
+    allFlashing();
+    break;
+  case 3:
+    allBounce();
+    Serial.println("case 3 ");
+    break;
+  }
 }
 
-void allon() {
+void allOn() {
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, HIGH);
   }
 }
 
-void alloff(){
+void allOff(){
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, LOW);
   }
 }
 
-void allflashing(){
+void allFlashing(){
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, flash);
   }
 }
 
-void allbounce(){
+void allBounce(){
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  for(int p = 0; true; p++ ){
+    int pmod = p % 8;
+    if (pmod>4){
+      pmod = 8 - pmod;}
+    pmod = 9 + pmod;
+    digitalWrite(pmod, HIGH);
+    delay(bouncetime);
+    digitalWrite(pmod, LOW);
+  }
 }
