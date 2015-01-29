9b49d4e6ad54120769892fc6c7b128372fd42a10
Antonia Elsen <antoniaelsen@gmail.com>
2015-01-29 15:03:54 -0500

    Debounce button, clean and comment code
    Debounce button with millis() (previously was delay() )
    Add comments
    Fix syntax

--- Lab1_bikelight/Lab1_bikelight.ino
+++ Lab1_bikelight/Lab1_bikelight.ino
@@ -1,11 +1,21 @@
+/**
+Principles of Engineering
+Spring 2015
+Lab 1 - Brake Light
+Antonia Elsen, Jessica Oehrlein, William Saulnier
 
+A small arduino script that will control 5 LEDs, making them flash
+in four different patterns -- all on, all off, all flashing, or all
+bouncing. The speed of the bouncing is controlled by the rotation
+of a 10k potentiometer.
+**/
+
+boolean flash;
 int button = 2;
 int bouncet = 0;
-volatile int switcher = 0;
-boolean flash;
-boolean isUp;
-int flashtime = 50;
-int bouncetime = 100;
+int flash_interval = 50;
+volatile int mode = 0;
+unsigned long last_inter = 0;
 
 
 void setup() {
@@ -15,18 +25,29 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
-  attachInterrupt(0, switcherf, RISING);
+  attachInterrupt(0, modef, RISING);
 }
 
-void switcherf() {
-  switcher++;
-  switcher = switcher % 4;
-  Serial.println(switcher);
-  delay(500);
+/**
+  Interrupt function that switches between the four modes of
+  LED flashing on button press.
+**/
+void modef() {
+  //Debounce the button. Ignore any interrupts after the first
+    //for 200ms after the first interrupt
+  unsigned long first_inter = millis();
+  if (first_inter - last_inter > 200){
+    mode++;
+    mode = mode % 4;
+  }
+  last_inter = first_inter;
 }
 
+/**
+
+**/
 void loop() {
-  switch (switcher){
+  switch (mode){
     case 0:
       allOn();
       break;
@@ -34,51 +55,65 @@ void loop() {
       allOff();
       break;
     case 2:
-      delay(flashtime);
+      delay(flash_interval);
       flash = !flash;
       allFlashing();
       break;
     case 3:
-      allBounce();
-      Serial.println("case 3 ");
+      allBouncing();
       break;
     }
 }
 
+/**
+  Turns all LEDs on.
+**/
 void allOn() {
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, HIGH);
   }
 }
 
+/**
+  Turns all LEDs off.
+**/
 void allOff(){
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, LOW);
   }
 }
 
+/**
+  Flashes the LEDs on or off, depending on their previous state.
+
+  Did you really need me to comment these?
+**/
 void allFlashing(){
   for(int p = 9; p<14; p++ ){
     digitalWrite(p, flash);
   }
 }
 
-
-//MAKE ME BOUNCE
-void allBounce(){
+/**
+  Switches one LED on at a time to create a bouncing pattern.
+  The rate at which the LEDs bounce (the delay) is determined from the voltage
+  read from a voltage divider on analog pin 0.
+**/
+void allBouncing(){
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
   digitalWrite(12, LOW);
   digitalWrite(13, LOW);
+
+
   int pmod = bouncet % 8;
   if (pmod>4){
     pmod = 8 - pmod;}
   pmod = 9 + pmod;
-  bouncet = pmod;
+  bouncet++;
+
   digitalWrite(pmod, HIGH);
-  bouncetime = analogRead(0);
-  delay(bouncetime);
+  delay(analogRead(0));
   digitalWrite(pmod, LOW);
-  }
 }
