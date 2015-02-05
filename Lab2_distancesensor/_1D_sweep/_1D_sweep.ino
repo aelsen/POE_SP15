860dd3be9fa3aac83b02697e12d791352d42b97c
Antonia Elsen <antoniaelsen@gmail.com>
2015-02-05 14:00:09 -0500

    create Lab 2 folder, calibration data, 1Dsweep.ino

---
+++ Lab2_distancesensor/_1D_sweep/_1D_sweep.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/**
+  Distance = -313.428369 * ln(x) _ 1189.047935
+**/
+double estimateDistance(){
+}
