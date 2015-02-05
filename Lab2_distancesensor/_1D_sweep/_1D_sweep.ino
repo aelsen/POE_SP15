2f22e222b5dee6e37c2f888a1b583d325fce6393
Antonia Elsen <antoniaelsen@gmail.com>
2015-02-05 14:27:43 -0500

    add sensor and servo control

--- Lab2_distancesensor/_1D_sweep/_1D_sweep.ino
+++ Lab2_distancesensor/_1D_sweep/_1D_sweep.ino
@@ -1,15 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myservo;
+
+int sensorPin = A0; 
+int sensorValue = 0;
+int distanceEst = 0;
+int pos = 0;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position
 
+
+    //measure angle 
+    sensorValue = analogRead(sensorPin);
+    distanceEst = estimateDistance(sensorValue);
+    Serial.print("angle: "); 
+    Serial.print(pos); 
+    Serial.print(", raw input: "); 
+    Serial.print(sensorValue); 
+    Serial.print(", distance: "); 
+    Serial.println(distanceEst);
+  } 
 }
 
 /**
-  Distance = -313.428369 * ln(x) _ 1189.047935
+  Distance = -313.428369 * ln(x) + 1189.047935
 **/
-double estimateDistance(){
+double estimateDistance(int pos){
+	double estimate = -1;
+	estimate = -313.428 * log(pos) + 1189.048;
+	return estimate;
 }
